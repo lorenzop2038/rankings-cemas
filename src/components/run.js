8d32d75e79e41da8fab9cd6223adc18e32088be7
lorenzo_6toD_6 <lorenzo_perez_cemas@gmail.com>
2021-04-30 13:17:38 -0400

    Agregando el Run

---
+++ src/components/run.js
@@ -0,0 +1,22 @@
+const run = () => {
+    const select = document.getElementById
+    ("select_test") .value;
+
+    switch (select) {
+        case "op_1":
+            // Codigo para este caso...
+            break;
+        case "op2":
+            // Codigo para este caso...
+            break;
+        case "op3":
+            // Codigo para este caso...
+            break;
+        case "op4":
+            // Codigo para este caso...
+             break;
+
+        default:
+            return null;
+    }
+};
